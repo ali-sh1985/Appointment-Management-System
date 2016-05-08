2593879596950853584d028c32540158e7a8a603
ali-sh1985 <ali.sh1985@gmail.com>
2016-05-08 17:10:43 -0500

    Add Domain Model

--- src/main/java/com/cs4/appointmentManagement/domain/Specialty.java
+++ src/main/java/com/cs4/appointmentManagement/domain/Specialty.java
@@ -1,5 +1,17 @@
 package com.cs4.appointmentManagement.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Specialty {
 
+	@Id
+	@GeneratedValue
+	private Long Id;
+	
+	@Column(name="Specialty_Name")
+	private String name;
 }
