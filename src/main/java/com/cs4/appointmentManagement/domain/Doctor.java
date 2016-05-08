29250ec3318ab30f16ad9bb4dd6ddb44a6acdbfa
ali-sh1985 <ali.sh1985@gmail.com>
2016-05-08 16:58:56 -0500

    Add Domain Model

--- src/main/java/com/cs4/appointmentManagement/domain/Doctor.java
+++ src/main/java/com/cs4/appointmentManagement/domain/Doctor.java
@@ -1,8 +1,23 @@
 package com.cs4.appointmentManagement.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Doctor {
-
+	
+	@Id
+	@GeneratedValue
+	private Long Id;
+	
+	@Column(name="First_Name")
+	private String firstName;
+	
+	@Column(name="Last_Name")
+	private String lastName;
+	
+	private Specialty specialty;
+	
 }
