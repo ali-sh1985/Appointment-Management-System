fdc6034cbe4aedde3fe0ec91c4019b9f78d20c62
ali-sh1985 <ali.sh1985@gmail.com>
2016-05-08 17:34:37 -0500

    Add Domain Model

--- src/main/java/com/cs4/appointmentManagement/domain/Doctor.java
+++ src/main/java/com/cs4/appointmentManagement/domain/Doctor.java
@@ -1,10 +1,13 @@
 package com.cs4.appointmentManagement.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -22,5 +25,8 @@ public class Doctor {
 	
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Specialty specialty;
+	
+	@ManyToMany(mappedBy = "doctors")
+	private List<Patient> patients;
 	
 }
---
+++ src/main/java/com/cs4/appointmentManagement/domain/Patient.java
@@ -0,0 +1,30 @@
+package com.cs4.appointmentManagement.domain;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Patient {
+
+	@Id
+	@GeneratedValue
+	private Long Id;
+
+	@Column(name = "First_Name")
+	private String firstName;
+
+	@Column(name = "Last_Name")
+	private String lastName;
+
+	@ManyToMany(cascade = CascadeType.PERSIST)
+	@JoinTable(name = "Patient_Doctor", joinColumns = @JoinColumn(name = "Patient_ID"), inverseJoinColumns = @JoinColumn(name = "Doctor_ID"))
+	private List<Doctor> doctors;
+}
