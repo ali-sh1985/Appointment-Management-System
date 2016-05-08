dff77183919a5f91e9983b3e4e350c576548d58f
ali-sh1985 <ali.sh1985@gmail.com>
2016-05-08 15:55:19 -0500

    First Commit

---
+++ src/main/java/com/cs4/appointmentManagement/EaProjectApplication.java
@@ -0,0 +1,12 @@
+package com.cs4.appointmentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/cs4/appointmentManagement/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cs4.appointmentManagement;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EaProjectApplication.class);
+	}
+
+}
