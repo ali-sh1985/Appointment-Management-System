a5cbc28c52ff6f15695c3143d6724d61c028441a
tek <tek.b.ale@gmail.com>
2016-05-17 01:14:37 -0500

    login done

--- src/main/java/com/cs4/appointmentManagement/dao/impl/UserCredentialsDaoImpl.java
+++ src/main/java/com/cs4/appointmentManagement/dao/impl/UserCredentialsDaoImpl.java
@@ -1,13 +1,24 @@
 package com.cs4.appointmentManagement.dao.impl;
 
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
 import com.cs4.appointmentManagement.dao.UserCredentialsDao;
+import com.cs4.appointmentManagement.domain.User;
 import com.cs4.appointmentManagement.domain.UserCredentials;
 
 @Repository
 public class UserCredentialsDaoImpl extends GenericDaoImpl<UserCredentials> implements UserCredentialsDao{
 	public UserCredentialsDaoImpl() {
 		super.setDaoType(UserCredentials.class);
+	}
+
+	@Override
+	public UserCredentials findByUsername(String username) {
+		Query query = entityManager.createQuery("SELECT u FROM "+daoType.getName()+" u INNER JOIN u.authority a WHERE "
+				+ "u.userName = :username)");
+		query.setParameter("username", username);
+		return (UserCredentials) query.getSingleResult();
 	}
 }
--- src/main/java/com/cs4/appointmentManagement/service/UserCredentialsService.java
+++ src/main/java/com/cs4/appointmentManagement/service/UserCredentialsService.java
@@ -7,11 +7,13 @@ import com.cs4.appointmentManagement.domain.UserCredentials;
 public interface UserCredentialsService {
 	void save(UserCredentials userCredentials);
 
-    void delete(Long id);
+	void delete(Long id);
 
-    UserCredentials findOne(Long id);
+	UserCredentials findOne(Long id);
 
-    UserCredentials update(UserCredentials userCredentials);   
-    
-    List<UserCredentials> findAll();
+	UserCredentials update(UserCredentials userCredentials);
+
+	List<UserCredentials> findAll();
+
+	public UserCredentials findByUsername(String username);
 }
