33d5e2594df289d6fcf6aaeb13d56dd4142fa94d
tek <tek.b.ale@gmail.com>
2016-05-16 23:08:43 -0500

    login v2

--- src/main/java/com/cs4/appointmentManagement/service/impl/UserServiceImpl.java
+++ src/main/java/com/cs4/appointmentManagement/service/impl/UserServiceImpl.java
@@ -1,16 +1,20 @@
 package com.cs4.appointmentManagement.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.cs4.appointmentManagement.dao.UserDao;
 import com.cs4.appointmentManagement.domain.User;
 import com.cs4.appointmentManagement.service.UserService;
 
 @Component
 public class UserServiceImpl implements UserService {
-
+	@Autowired
+	private UserDao userDao;
+	
 	@Override
 	public User findByUsername(String username) {
-		return null;
+		return userDao.findByUsername(username);
 	}
 
 }
