67df61da62df267c7dcf75bf0c4b03fab04c5533
ali-sh1985 <ali.sh1985@gmail.com>
2016-05-16 23:16:54 -0500

    Local changes

--- src/main/java/com/cs4/appointmentManagement/service/UserService.java
+++ src/main/java/com/cs4/appointmentManagement/service/UserService.java
@@ -1,22 +1,9 @@
 package com.cs4.appointmentManagement.service;
 
-<<<<<<< HEAD
-
-import java.util.List;
-=======
->>>>>>> 33d5e2594df289d6fcf6aaeb13d56dd4142fa94d
 
 import com.cs4.appointmentManagement.domain.User;
 
 public interface UserService {
 	public User findByUsername(String username);
 
-<<<<<<< HEAD
-    User update(User user);   
-    
-    List<User> findAll();
-    
-    User findByUsername(String username);
-=======
->>>>>>> 33d5e2594df289d6fcf6aaeb13d56dd4142fa94d
 }
--- src/main/java/com/cs4/appointmentManagement/service/impl/UserServiceImpl.java
+++ src/main/java/com/cs4/appointmentManagement/service/impl/UserServiceImpl.java
@@ -1,45 +1,5 @@
 package com.cs4.appointmentManagement.service.impl;
 
-<<<<<<< HEAD
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.cs4.appointmentManagement.dao.UserDao;
-import com.cs4.appointmentManagement.domain.Speciality;
-import com.cs4.appointmentManagement.domain.User;
-import com.cs4.appointmentManagement.service.UserService;
-
-@Service
-@Transactional
-public class UserServiceImpl implements UserService{
-
-	@Autowired
-	private UserDao userDao;
-	
-	@Override
-	public void save(User speciality) {
-		userDao.save(speciality);
-	}
-
-	@Override
-	public void delete(Long id) {
-		userDao.delete(id);
-	}
-
-	@Override
-	public User findOne(Long id) {
-		return userDao.findOne(id);
-	}
-
-	@Override
-	public User update(User user) {
-		return userDao.update(user);
-	}
-
-=======
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -52,11 +12,6 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserDao userDao;
 	
->>>>>>> 33d5e2594df289d6fcf6aaeb13d56dd4142fa94d
-	@Override
-	public List<User> findAll() {
-		return (List<User>)userDao.findAll();
-	}
 	
 	@Override
 	public User findByUsername(String username) {
