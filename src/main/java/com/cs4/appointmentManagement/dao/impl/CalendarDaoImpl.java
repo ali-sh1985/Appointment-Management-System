70a5aec485b5b144b2237029265d0f6e999c5ed4
ali-sh1985 <ali.sh1985@gmail.com>
2016-05-16 23:21:22 -0500

    small fix.

--- src/main/java/com/cs4/appointmentManagement/dao/impl/CalendarDaoImpl.java
+++ src/main/java/com/cs4/appointmentManagement/dao/impl/CalendarDaoImpl.java
@@ -1,7 +1,10 @@
 package com.cs4.appointmentManagement.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.cs4.appointmentManagement.domain.Calendar;
 
+@Repository
 public class CalendarDaoImpl extends GenericDaoImpl<Calendar>{
 
 }
